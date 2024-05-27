0514531f634c2993d71e5c2f0b69614f8befb71b
haikalrfli11 <haikalrafli11@gmail.com>
2024-05-27 21:05:33 +0700

    add new codeforces code solution

---
+++ 800/Problem A/41A_Translation/41A_Translation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+      
+  string t;
+  string u;
+  cin >> t;
+  cin >> u;
+  string s = "";
+  for(int i = t.length()-1; i>=0 ;i--) {
+    s+= t[i];
+  }
+
+  if(s == u) {
+  cout << "YES";
+}else {
+  cout << "NO";
+}
+
+    return 0;
+}
+
+
