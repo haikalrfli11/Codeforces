ab8a097146470b7e0546a4b23dc7334f5947e4c1
haikalrfli11 <haikalrafli11@gmail.com>
2024-05-28 10:25:22 +0700

    add third problem

---
+++ 800/Problem A/50A_Domino_Piling/50A_Domino_Piling.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+      int m,n; cin >> m >> n; 
+      cout << (m * n)/2;
+      }
+     
